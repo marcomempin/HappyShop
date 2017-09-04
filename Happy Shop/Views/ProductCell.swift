8f9720f55851def5a17f5b8fceb077be2b587ed4
Marco Mempin <marcomempin@gmail.com>
2017-09-04 17:31:39 +0800

    Add ProductCell swift and xib files

---
+++ Happy Shop/Views/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  Happy Shop
+//
+//  Created by Marco Mempin on 9/4/17.
+//  Copyright © 2017 marcowesome. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var onSaleLabel: UILabel!
+}
