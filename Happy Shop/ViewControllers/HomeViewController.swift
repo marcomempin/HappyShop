3cb8454c6d93e869604df67ee665569295a3bddf
Marco Mempin <marcomempin@gmail.com>
2017-09-04 17:19:15 +0800

    Add HomeViewController with a UICollectionView

---
+++ Happy Shop/ViewControllers/HomeViewController.swift
@@ -0,0 +1,63 @@
+//
+//  HomeViewController.swift
+//  Happy Shop
+//
+//  Created by Marco Mempin on 9/4/17.
+//  Copyright © 2017 marcowesome. All rights reserved.
+//
+
+import UIKit
+import IGListKit
+
+class HomeViewController: UIViewController {
+    
+    // MARK: Data
+    let categoryLoader = CategoryLoader()
+    var categories = [Category]()
+    
+    // MARK: IGListKit
+    lazy var adapter: ListAdapter = {
+        return ListAdapter(updater: ListAdapterUpdater(), viewController: self, workingRangeSize: 0)
+    }()
+    
+    // MARK: UI
+    let collectionView: UICollectionView = {
+        let view = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        view.backgroundColor = .white
+        return view
+    }()
+
+    // MARK: View Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Categories"
+        
+        self.categoryLoader.getCategories { 
+            self.categories = self.categoryLoader.categories
+            self.adapter.performUpdates(animated: true)
+        }
+        
+        view.addSubview(collectionView)
+        adapter.collectionView = collectionView
+        adapter.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView.frame = view.bounds
+    }
+}
+
+extension HomeViewController: ListAdapterDataSource {
+    func objects(for listAdapter: ListAdapter) -> [ListDiffable] {
+        return categories
+    }
+    
+    func listAdapter(_ listAdapter: ListAdapter, sectionControllerFor object: Any) -> ListSectionController {
+        return LabelSectionController()
+    }
+    
+    func emptyView(for listAdapter: ListAdapter) -> UIView? {
+        return nil
+    }
+}
