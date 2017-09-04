87b3d0032f516564e6c1b1a207e07a78b95789a5
Marco Mempin <marcomempin@gmail.com>
2017-09-04 17:19:55 +0800

    Add LabelCell swift and xib files

---
+++ Happy Shop/Views/LabelCell.swift
@@ -0,0 +1,14 @@
+//
+//  LabelCell.swift
+//  Happy Shop
+//
+//  Created by Marco Mempin on 9/4/17.
+//  Copyright © 2017 marcowesome. All rights reserved.
+//
+
+import UIKit
+
+class LabelCell: UICollectionViewCell {
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var countLabel: UILabel!
+}
