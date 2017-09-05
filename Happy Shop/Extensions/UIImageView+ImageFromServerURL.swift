75a09c83371041c7722396a6f05e56baf3948dd2
Marco Mempin <marcomempin@gmail.com>
2017-09-05 14:20:28 +0800

    Move imageFromServerURL function to its own file

--- Happy Shop/Extensions/UIImageView+ImageFromServerURL.swift
+++ Happy Shop/Extensions/UIImageView+ImageFromServerURL.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+import UIKit
+
+// http://stackoverflow.com/a/37019507/737370
+extension UIImageView {
+    public func imageFromServerURL(urlString: String, completion: @escaping () -> Void) {
+        
+        URLSession.shared.dataTask(with: NSURL(string: urlString)! as URL, completionHandler: { (data, response, error) -> Void in
+            
+            if error != nil {
+                print(error!)
+                completion()
+                return
+            }
+            DispatchQueue.main.async(execute: { () -> Void in
+                let image = UIImage(data: data!)
+                self.image = image
+                completion()
+            })
+            
+        }).resume()
+    }
+}
