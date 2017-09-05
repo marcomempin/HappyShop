80796b84b1ecb08c78e07e5ebaf4a9c2bd01e56a
Marco Mempin <marcomempin@gmail.com>
2017-09-05 14:21:23 +0800

    Add randomHexColorCode function for the placeholder image’s background and font color

--- Happy Shop/Extensions/String+RandomHexColorCode.swift
+++ Happy Shop/Extensions/String+RandomHexColorCode.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+
+extension String {
+    // http://www.amarendrasingh.in/swift/swift-3-uicolor-utilities-random-hex-color-codes-random-uicolor/
+    static func randomHexColorCode() -> String {
+        let a = ["1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
+        return a[Int(arc4random_uniform(15))]
+            .appending(a[Int(arc4random_uniform(15))])
+            .appending(a[Int(arc4random_uniform(15))])
+            .appending(a[Int(arc4random_uniform(15))])
+            .appending(a[Int(arc4random_uniform(15))])
+            .appending(a[Int(arc4random_uniform(15))])
+    }
+}
