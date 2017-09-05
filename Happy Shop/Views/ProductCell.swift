6cace79915017a20cfd3a2938700d4aedf5b532a
Marco Mempin <marcomempin@gmail.com>
2017-09-05 14:23:24 +0800

    Add fileprivate static constants Add textHeight function for ProductCell’s height calculation in ProductView

--- Happy Shop/Views/ProductCell.swift
+++ Happy Shop/Views/ProductCell.swift
@@ -14,4 +14,16 @@ class ProductCell: UICollectionViewCell {
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var onSaleLabel: UILabel!
     @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+    
+    fileprivate static let insets = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+    fileprivate static let font = UIFont.systemFont(ofSize: 16)
+    fileprivate static let priceAndSaleLabelHeight: CGFloat = 30
+    
+    static func textHeight(_ text: String, width: CGFloat) -> CGFloat {
+        let constrainedSize = CGSize(width: width - insets.left - insets.right, height: CGFloat.greatestFiniteMagnitude)
+        let attributes = [ NSFontAttributeName: font ]
+        let options: NSStringDrawingOptions = [.usesFontLeading, .usesLineFragmentOrigin]
+        let bounds = (text as NSString).boundingRect(with: constrainedSize, options: options, attributes: attributes, context: nil)
+        return ceil(bounds.height) + insets.top + insets.bottom + priceAndSaleLabelHeight
+    }
 }
